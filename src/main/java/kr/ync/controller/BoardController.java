5766ac3c36952079e0728ede2f60cbbb4c1d7730
jcwoo <jcwoo1213@gmail.com>
2020-11-25 23:29:49 +0900

    test2

--- src/main/java/kr/ync/controller/BoardController.java
+++ src/main/java/kr/ync/controller/BoardController.java
@@ -12,7 +12,7 @@
 //import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.multipart.MultipartFile;
 //import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//
+
 //import kr.ync.domain.BoardVO;
 //import kr.ync.domain.Criteria;
 //import kr.ync.domain.PageDTO;
